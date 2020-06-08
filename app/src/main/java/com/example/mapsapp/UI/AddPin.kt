caa0994e4b9b10796186541e0db3c30e2b95528e
AngeloC98 <acabrasnl@gmail.com>
2020-06-08 14:09:41 +0200

    Updated add pin layout, and added geocoder for converting to address

--- app/src/main/java/com/example/mapsapp/UI/AddPin.kt
+++ app/src/main/java/com/example/mapsapp/UI/AddPin.kt
@@ -1,13 +1,54 @@
 package com.example.mapsapp.UI
 
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.example.mapsapp.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_add_pin.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class AddPin : AppCompatActivity() {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_pin)
+
+        initViews()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initViews() {
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                val latitude = location?.latitude
+                val longitude = location?.longitude
+                val address = getAddress(latitude!!, longitude!!)
+                tvLocation.text = address
+            }
+        // Get date and format
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        val formattedDate = current.format(formatter)
+
+        tvDate.text = formattedDate
+
+    }
+
+    private fun getAddress(lat: Double, lng: Double): String {
+        val geocoder = Geocoder(this)
+        val list = geocoder.getFromLocation(lat, lng, 1)
+        return list[0].getAddressLine(0)
     }
 }
--- app/src/main/java/com/example/mapsapp/UI/MapsActivity.kt
+++ app/src/main/java/com/example/mapsapp/UI/MapsActivity.kt
@@ -1,24 +1,40 @@
 package com.example.mapsapp.UI
 
 import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.mapsapp.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_maps.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private val FIREBASE_REQUEST_CODE = 1
+
+    lateinit var providers: List<AuthUI.IdpConfig>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,31 +44,79 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun initViews() {
+        providers = listOf<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val intent = Intent(this, AddPin::class.java)
+            startActivity(intent)
         }
+
+        showSignInOptions()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
+            == PackageManager.PERMISSION_GRANTED
+        ) {
             mMap.isMyLocationEnabled = true
         } else {
             // Permission to access the location is missing. Show rationale and request permission
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE
+            )
         }
 
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                val latitude = location?.latitude
+                val longitude = location?.longitude
+                mMap.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(
+                            latitude!!,
+                            longitude!!
+                        ), 16F
+                    )
+                )
+            }
 
-        val sydney = LatLng(-34.0, 151.0)
         //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    private fun showSignInOptions() {
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.AppTheme)
+                .build(), FIREBASE_REQUEST_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FIREBASE_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                if (user != null) {
+                    Toast.makeText(this, "" + user.email, Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "" + response!!.error!!, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
 }
